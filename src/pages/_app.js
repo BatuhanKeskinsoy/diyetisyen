01c453635ba49d6711d05bfdf1adf9bda6dd6c0f
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-10-24 22:11:02 +0300

    fix problems v1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,73 @@
 import React from 'react'
+import Head from 'next/head'
 import Footer from '@/components/inc/Footer/Footer'
 import Header from '@/components/inc/Header/Header'
 import AutoScrollTop from '@/components/Other/AutoScrollTop'
 import FixedScrollTop from '@/components/Other/FixedScrollTop'
+import generalsData from '@/data/generals'
 import '@/styles/globals.css'
 import '@/styles/slick-theme.min.css'
 import '@/styles/slick.min.css'
 
 export default function App({ Component, pageProps }) {
+
+  const siteName = process.env.NEXT_PUBLIC_SITE_NAME
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  
   return (
     <>
+      <Head>
+        {/* Base Meta */}
+        <title>{`Diyetisyen | ${siteName}`}</title>
+        <meta name="description" content="En iyi diyetisyeni bulun ve uzman diyetisyen desteği alın" />
+        <meta name="keywords" content="samsun diyetisyen, atakum diyetisyen, türkiş diyetisyen" />
+        <meta name="robots" content="index, follow" />
+        <meta rel="canonical" content={siteUrl} />
+        <meta name="siteurl" content={siteUrl} />
+        <meta name="creator" content="Batuhan Keskinsoy" />
+        <meta name="author" content={generalsData.fullName} />
+
+        <meta property="og:title" content={`Diyetisyen | ${siteName}`} />
+        <meta property="og:description" content="En iyi diyetisyeni bulun ve uzman diyetisyen desteği alın" />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="tr_TR" />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:image" content={`${siteUrl}/logo/logo.svg`} />
+
+        <meta name="twitter:site" content="@batuhankesk" />
+        <meta name="twitter:creator" content={generalsData.fullName} />
+        <meta name="twitter:domain" content={siteUrl} />
+        <meta name="twitter:title" content={`Diyetisyen | ${siteName}`} />
+        <meta name="twitter:description" content="En iyi diyetisyeni bulun ve uzman diyetisyen desteği alın" />
+        <meta name="twitter:image" content={`${siteUrl}/logo/logo.svg`} />
+
+        <meta property="article:author" content="https://www.facebook.com/batuhankeskinsoy" />
+        <meta property="article:publisher" content="https://www.facebook.com/batuhankeskinsoy" />
+
+        {/* Base Meta End */}
+
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* Favicon End */}
+      </Head>
+
       <AutoScrollTop />
       <Header />
       <main>
