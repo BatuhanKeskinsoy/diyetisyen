4d4529c3f88edb677bee83e1b88f8615c57556ff
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-11-06 19:19:47 +0300

    gallery zoom ( react awesome lightbox )

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import galleryPhotosData from '@/data/galleryPhotos';
 import Item from './Item';
+import servicesData from '@/data/services'
+import Lightbox from 'react-awesome-lightbox';
+import 'react-awesome-lightbox/build/style.css';
 
 function Gallery(props) {
+
+    const hizmetler = servicesData.map((item) => item.title).join(', ')
+
+    const [lightboxOpen, setLightboxOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const openLightbox = (index) => {
+        setCurrentImage(index);
+        setLightboxOpen(true);
+    };
+
+    const closeLightbox = () => {
+        setLightboxOpen(false);
+    };
 
     const Slider = props.Slider
 
@@ -44,30 +61,35 @@ function Gallery(props) {
             }
         ]
     };
+
     return (
         <section id='gallerySection' className="relative bg-gray-100">
             <div className='container mx-auto lg:px-0 px-4 lg:py-24 pt-14 pb-24 gap-y-8 flex flex-col'>
                 <div className="flex flex-col gap-y-3">
                     <h2 className='text-3xl lg:text-5xl font-dancing tracking-wide text-center'>Fotoğraf Galerisi</h2>
-                    <p className='text-lg text-gray-500 text-center'>Andulasyon Terapi, Emsculpt Neo ve diyet programlarımız ile ideal kilolarına ulaşmak isteyen danışanlarımızın sonuçları</p>
+                    <p className='text-base lg:text-lg text-gray-500 text-center'>{hizmetler} ve diyet programlarımız ile ideal kilolarına ulaşmak isteyen danışanlarımızın sonuçları</p>
                 </div>
                 <Slider {...settings}>
-
-                    {
-                        galleryPhotosData.map((galleryItem, key) => (
-
-                            <Item
-                                key={key}
-                                image={galleryItem.image}
-                                alt={galleryItem.alt} />
-
-                        ))
-                    }
-
+                    {galleryPhotosData.map((galleryItem, index) => (
+                        <Item
+                            key={index}
+                            image={galleryItem.image}
+                            alt={galleryItem.alt}
+                            openLightbox={() => openLightbox(index)}
+                        />
+                    ))}
                 </Slider>
             </div>
+
+            {lightboxOpen && (
+                <Lightbox
+                    images={galleryPhotosData.map((item) => ({ url: item.image, title: item.alt }))}
+                    onClose={closeLightbox}
+                    startIndex={currentImage}
+                />
+            )}
         </section>
     )
 }
 
-export default Gallery
+export default Gallery;
