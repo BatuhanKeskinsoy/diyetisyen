c1744d69e9062734fad9642a9225868d1095134d
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-11-06 18:37:44 +0300

    gallery design fix

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -12,7 +12,7 @@ function Gallery(props) {
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         lazyLoad: true,
         autoplay: true,
         autoplaySpeed: 4000,
@@ -21,14 +21,14 @@ function Gallery(props) {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 1,
                     arrows: false,
                     dots: true,
                 }
@@ -47,7 +47,10 @@ function Gallery(props) {
     return (
         <section id='gallerySection' className="relative bg-gray-100">
             <div className='container mx-auto lg:px-0 px-4 lg:py-24 pt-14 pb-24 gap-y-8 flex flex-col'>
-                <h2 className='text-3xl lg:text-5xl font-dancing tracking-wide'>Fotoğraf Galerisi</h2>
+                <div className="flex flex-col gap-y-3">
+                    <h2 className='text-3xl lg:text-5xl font-dancing tracking-wide text-center'>Fotoğraf Galerisi</h2>
+                    <p className='text-lg text-gray-500 text-center'>Andulasyon Terapi, Emsculpt Neo ve diyet programlarımız ile ideal kilolarına ulaşmak isteyen danışanlarımızın sonuçları</p>
+                </div>
                 <Slider {...settings}>
 
                     {
