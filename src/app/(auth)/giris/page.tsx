f0b1ae72256c45bd70fee36db61931b0c0f4d8cf
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2024-02-10 21:42:42 +0300

    Update page.tsx

--- src/app/(auth)/giris/page.tsx
+++ src/app/(auth)/giris/page.tsx
@@ -14,12 +14,12 @@ function page() {
         fill
         className="object-cover object-bottom"
       />
-      <div className="flex items-center justify-center min-h-screen bg-site/20 p-4 backdrop-blur">
-        <div className="flex flex-col w-full max-w-xl shadow-lg rounded-xl bg-white/70 shadow-black/20 lg:p-12 md:p-8 p-4 overflow-hidden">
-          <h1 className="text-4xl font-semibold font-gemunu text-site">
+      <div className="flex items-center justify-center h-[100dvh] max-h-[100dvh] bg-black-900/20 p-4 backdrop-blur-md">
+        <div className="flex flex-col w-full max-w-xl shadow-lg rounded-xl bg-white/70 shadow-black/20 lg:p-12 p-8 overflow-hidden">
+          <h1 className="text-4xl font-semibold font-gemunu text-[#002a4f]">
             Giriş Yap
           </h1>
-          <hr className="my-6 opacity-20" />
+          <hr className="my-6 border-gray-600 opacity-15" />
           <LoginForm />
         </div>
       </div>
